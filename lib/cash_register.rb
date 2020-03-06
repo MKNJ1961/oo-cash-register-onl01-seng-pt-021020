5da06744eb459ad0a1d113e3b42c432815f9662c
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-06 01:25:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,14 +8,14 @@ class CashRegister
 
   end
 
-  def apply_discount
-    if discount != 0
-      @total -= (@total * (@discount / 100.to_f))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
-      "There is no discount to apply."
-    end
-  end
+  # def apply_discount
+  #   if discount != 0
+  #     @total -= (@total * (@discount / 100.to_f))
+  #     "After the discount, the total comes to $#{@total.to_i}."
+  #   else
+  #     "There is no discount to apply."
+  #   end
+  # end
 
 #   def add_item(title, price, quantity=1)
 #    @total += price * quantity
