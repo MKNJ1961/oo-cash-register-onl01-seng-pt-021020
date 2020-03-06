ed3f864748a1eca2ed83fc845d602344d0cea545
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-06 01:25:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,20 +17,20 @@ class CashRegister
     end
   end
 
-  def add_item(title, price, quantity=1)
-   @total += price * quantity
-   quantity.times do
-     @items << title
-   end
-   @last_item = price * quantity
-  end
-
-  def items
-    @items
-  end
-
-  def void_last_transaction
-     @items.delete_at(-1)
-    self.total = self.total - @last_item
-  end
-end
+#   def add_item(title, price, quantity=1)
+#    @total += price * quantity
+#    quantity.times do
+#      @items << title
+#    end
+#    @last_item = price * quantity
+#   end
+# 
+#   def items
+#     @items
+#   end
+# 
+#   def void_last_transaction
+#      @items.delete_at(-1)
+#     self.total = self.total - @last_item
+#   end
+# end
